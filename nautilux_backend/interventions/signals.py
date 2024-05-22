dec819a37d80e3aee331ea13a7fa2be43e2a08e3
Paula <paula.rios.a@gmail.com>
2024-05-22 17:25:14 +0200

    Des commentaires explicatifs ont été ajoutés au code frontend et backend

--- nautilux_backend/interventions/signals.py
+++ nautilux_backend/interventions/signals.py
@@ -1,14 +1,17 @@
+# coding=utf-8
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from interventions.models import Intervention
 
 
+# Récepteur pour le signal post_save du modèle Intervention, exécuté à chaque fois qu'une sauvegarde est effectuée
 @receiver(post_save, sender=Intervention)
 def update_statut(sender, instance, **kwargs):
+    # Vérifier si l'intervention est en statut "Brouillon" et si tous les champs facultatifs sont remplis
     if (instance.statut == Intervention.BROUILLON and instance.libelle
             and instance.description
             and instance.nom_intervenant
             and instance.date_intervention):
-        # Statut should be changed
+        # Si toutes les conditions sont remplies, le statut doit être changé à "Validé"
         Intervention.objects.filter(id=instance.id).update(statut=Intervention.VALIDE)
--- nautilux_backend/interventions/views.py
+++ nautilux_backend/interventions/views.py
@@ -10,6 +10,7 @@ from interventions.serializers import InterventionSerializer, VilleSerializer, I
 
 
 def verify_and_update_statuts():
+    # Metre à jour le statut à 'Terminé' pour les interventions passées
     today = timezone.localdate()
     Intervention.objects.exclude(statut=Intervention.TERMINE).filter(
         date_intervention__lt=today,
@@ -22,19 +23,25 @@ class InterventionViewSet(viewsets.ModelViewSet):
     filter_backends = [filters.OrderingFilter]
     ordering_fields = ['date_creation']
 
+    # Redéfinition de la méthode list pour vérifier et mettre à jour les statuts avant de lister les interventions
     def list(self, request, *args, **kwargs):
+        # TODO: envisagez d'effectuer cette action en tant que tâche périodique en utilisant cron
         verify_and_update_statuts()
         return super(InterventionViewSet, self).list(request, *args, **kwargs)
 
+    # Redéfinition de la méthode update pour empêcher la modification des interventions terminées
     def update(self, request, *args, **kwargs):
         instance = self.get_object()
         if instance.statut == Intervention.TERMINE:
             raise PermissionDenied(detail="L'intervention n'est plus modifiable")
         return super(InterventionViewSet, self).update(request, *args, **kwargs)
 
+    # Sélectionner le sérialiseur en fonction de l'action
     def get_serializer_class(self):
+        # InterventionReadSerializer pour les actions de lecture (list, retrieve)
         if self.action == 'list' or self.action == 'retrieve':
             return InterventionReadSerializer
+        # Le sérialiseur par défaut (InterventionSerializer) pour les actions d'écriture
         return self.serializer_class
 
 
